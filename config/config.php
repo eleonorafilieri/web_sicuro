a60588aae2a896530b0c47029a4a1120aab7bb40
eleonora <eleonorafil21@gmail.com>
2023-06-11 16:26:43 +0200

    aaa

--- config/config.php
+++ config/config.php
@@ -1,16 +1,8 @@
 <?php
-	define("MSG_ERROR_LOGIN", "login_error");
-	define("MSG_LOGOUT", "logout_successful");
-	define("MSG_ERROR_ORDER", "order_error");
-	
-	$dbhost = "localhost";
-	$dbuser = "root";
-	$dbpwd = "";
-	$dbname = "websicuro";
-	
-	$errori = array(
-		'login_error' => '<div class="alert alert-danger"><p>Username o password errati</p></div>',
-		'logout_successful' => '<div class="alert alert-success"><p>Logout effettuato con successo</p></div>',
-		'order_error' => '<div class="alert alert-danger"><p>Ordine non registrato</p></div>',
-	);
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'negozio_piante');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('LOGIN_ERROR', 'loginError');
+define('LOGOUT_SUCCESS', 'logoutSuccess');
 ?>
--- index.php
+++ index.php
@@ -1,40 +1,99 @@
 <?php
-	require 'common/elements/header.php';
-	//ciao
-?>
-	<div class="row">
-		<div class="col-sm-12">
-<?php
-	if (!$logged) {
-?>
-		<h2>Login</h2>
-		<form id="loginForm" method="post" action="common/dialog_manager.php?action=login">
-			<p>
-				<label for="username">Username </label>
-				<br/>
-				<input id="username" name="username" type="text" maxlength="255" size="30" value=""/>				
-			</p>
-			<p>
-				<label for="pwd">Password </label>
-				<br />
-				<input id="pwd" name="pwd" type="password" maxlength="255" size="30" value=""/>
-			</p>
-			<p>
-				<input class="btn btn-primary" type="submit" name="submit" value="Login" />
-			</p>
-		</form>
-<?php
-	}
-	else {
-?> 
-	<h2>Ordina una delle nostre specialità</h2>
-<?php
-	echo showOrder($user_id);
-	echo printGrid();
-	}
-?>
-		</div>
-	</div>
-<?php
-	require 'common/elements/footer.php';
-?>	
+session_start();
+include 'functions/db.php';
+include 'functions/functions.php';
+$families = getPlantFamilies();
+
+$selectedFamilyId = isset($_GET['famiglia']) ? $_GET['famiglia'] : null;
+$plants = ($selectedFamilyId !== null) ? getPlantsByFamily($selectedFamilyId) : getAllPlants();
+if (isset($_GET['logout'])) {
+    session_unset();
+    session_destroy();
+    header('Location: index.php');
+    exit();
+}
+
+$user = isset($_SESSION['user']) ? $_SESSION['user'] : null;?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bloom</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <header>
+     
+    <nav class="navbar navbar-expand-lg navbar-dark ">
+        <a class="navbar-brand" href="#">
+            <img src="elements/logo.png" alt="Logo" class="logo d-inline-block align-top">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav mr-3">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <?php if ($user) { ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?logout">Logout</a>
+                        </li>
+                    <?php } else { ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.php">Login</a>
+                        </li>
+                    <?php } ?>
+            </ul>
+        </div>
+    </nav>
+    </header>
+
+<main>
+    <div class="container">
+        <h1>Bloom - Il tuo negozio di fiori</h1>
+
+        <div class="row">
+            <div class="col-md-12">
+                <h3>Filtra per famiglia</h3>
+                <ul class="list-group d-flex flex-row">
+                    <li class="list-group-item <?php if ($selectedFamilyId === null) echo 'selected'; ?>">
+                        <a href="?">Tutte</a>
+                    </li>
+                    <?php
+                    foreach ($families as $family) {
+                        $class = ($selectedFamilyId == $family['id_famiglia']) ? 'selected' : '';
+                        echo '<li class="list-group-item ' . $class . '"><a href="?famiglia=' . $family['id_famiglia'] . '">' . $family['nome'] . '</a></li>';
+                    }
+                    ?>
+                </ul>
+            </div>
+        </div>
+
+        <div class="row">
+            <?php
+            foreach ($plants as $plant) {
+                echo '<div class="col-md-4">';
+                echo '    <div class="card">';
+                echo '        <img src="' . $plant['path_img'] . '" class="card-img-top" alt="' . $plant['titolo'] . '">';
+                echo '        <div class="card-body">';
+                echo '            <h5 class="card-title">' . $plant['titolo'] . '</h5>';
+                echo '            <p class="card-text">' . $plant['descrizione'] . '</p>';
+                echo '        </div>';
+                echo '    </div>';
+                echo '</div>';
+            }
+            ?>
+        </div>
+    </div>
+</main>
+
+<footer>
+        <p>&copy; 2023 Negozio di Piante</p>
+    </footer>
+</body>
+</html>
+
+
